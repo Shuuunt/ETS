22c610fe038bfd00c189a49d8e90c13bde846f11
Shuuunt <carlosevinah@gmail.com>
2025-09-24 21:58:55 +0200

    Fint commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Enlaza el botón con la página externa
+document.getElementById("miBoton").addEventListener("click", function() {
+  window.open("https://www.google.com", "_blank"); // abre en nueva pestaña
+});
